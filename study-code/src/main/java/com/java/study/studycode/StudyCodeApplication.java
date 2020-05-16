9037ada1b312439a0d690cbb3b3c6831e787e60d
cgm <1289206128@qq.com>
2020-05-17 00:26:36 +0800

    project init

---
+++ study-code/src/main/java/com/java/study/studycode/StudyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.java.study.studycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCodeApplication.class, args);
+    }
+
+}
