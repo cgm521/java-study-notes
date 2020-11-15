1593db7369b5cdb75a521d733de2ee1575fa54d7
cgm <1289206128@qq.com>
2020-11-15 17:55:09 +0800

    countDownLatch

--- study-code/src/main/java/com/java/study/studycode/common/mysql/ConnectionUtils.java
+++ study-code/src/main/java/com/java/study/studycode/common/mysql/ConnectionUtils.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 
 
 public class ConnectionUtils {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String USER = "root";
     private static final String PWD = "123456";
     private static final String URL = "jdbc:mysql://localhost:3306/test";
